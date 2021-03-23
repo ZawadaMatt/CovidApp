cb35abce540490d0ccdc2f1841de6ca951cf6a17
zawadamatt <mateusz.zawada10@gmail.com>
2021-03-23 11:30:57 +0100

    Base structure

---
+++ src/test/java/com/github/zawadamatt/CovidApp/CovidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zawadamatt.CovidApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
